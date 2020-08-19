aeb1728479b78512ba3b91dfa9aac38f26b05412
Greg Olsen <gregolsen701@gmail.com>
2020-08-18 22:25:11 -0500

    Add project files.

---
+++ DatingProfile.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace Lab7_4
+{
+    class DatingProfile
+    {
+        public string firstName;
+        private string lastName;
+        public int age;
+        public string gender;
+        public string bio;
+        public List<Messages> myMessages;
+
+        public DatingProfile(string firstName, string lastName, int age, string gender)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+            this.gender = gender;
+            myMessages = new List<Messages>();
+        }
+
+        public void WriteBio(string text)
+        {
+            bio = text;
+        }
+
+        public void ReadMessage()
+        {
+            foreach(Messages message in myMessages)
+            {
+                if (message.isRead == false)
+                {
+                    Console.WriteLine(message.messageTitle);
+                    Console.WriteLine(message.messageData);
+                    message.isRead = true;
+                }
+            }
+        }
+
+        public void AddToMessages(Messages message)
+        {
+            myMessages.Add(message);
+        }
+
+        public void SendMessage(string messageTitle, string messageData, DatingProfile toProfile)
+        {
+            Messages message = new Messages(this, messageTitle, messageData);
+            toProfile.AddToMessages(message);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lab7_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DatingProfile profile1 = new DatingProfile("Brad", "Johnson", 22, "male");
+            profile1.WriteBio("I Like Dogs.");
+
+            DatingProfile profile2 = new DatingProfile("Erica", "Bennet", 24, "female");
+            profile2.WriteBio("I Like Cats.");
+            profile1.SendMessage("Hello", "How are you?", profile2);
+            profile2.ReadMessage();
+        }
+    }
+}
